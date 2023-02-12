5e7f05adc63c8f7c439316d48f4e16b14677f80a
nstarman <nstarkman@protonmail.com>
2023-02-12 17:44:29 -0500

    Fix n_layers in lin_tanh
    Signed-off-by: nstarman <nstarkman@protonmail.com>

--- src/stream_ml/pytorch/nn.py
+++ src/stream_ml/pytorch/nn.py
@@ -11,13 +11,32 @@ __all__ = ["lin_tanh"]
 def lin_tanh(
     n_in: int = 1, n_hidden: int = 50, n_layers: int = 3, n_out: int = 3
 ) -> nn.Sequential:
-    """Linear tanh network."""
+    """Linear tanh network.
+
+    Parameters
+    ----------
+    n_in : int, optional
+        Number of input features, by default 1
+    n_hidden : int, optional
+        Number of hidden units, by default 50.
+    n_layers : int, optional
+        Number of hidden layers, by default 3.
+        Must be >= 2.
+    n_out : int, optional
+        Number of output features, by default 3.
+    """
+    midlayers = (
+        functools.reduce(
+            operator.add,
+            ((nn.Linear(n_hidden, n_hidden), nn.Tanh()) for _ in range(n_layers - 2)),
+        )
+        if n_layers >= 3  # noqa: PLR2004
+        else ()
+    )
+
     return nn.Sequential(
         nn.Linear(n_in, n_hidden),
         nn.Tanh(),
-        *functools.reduce(
-            operator.add,
-            ((nn.Linear(n_hidden, n_hidden), nn.Tanh()) for _ in range(n_layers - 2)),
-        ),
+        *midlayers,
         nn.Linear(n_hidden, n_out),
     )
