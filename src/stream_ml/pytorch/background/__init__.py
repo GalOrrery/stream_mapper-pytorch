c1c43f7c2cbd41ea5fac818453ab4be7f4ae7ef3
nstarman <nstarkman@protonmail.com>
2023-02-23 00:22:19 -0500

    move Uniform to core
    Signed-off-by: nstarman <nstarkman@protonmail.com>

--- src/stream_ml/pytorch/background/__init__.py
+++ src/stream_ml/pytorch/background/__init__.py
@@ -1,8 +1,18 @@
 """Background models."""
 
 
+from dataclasses import dataclass
+
+from stream_ml.core.background.uniform import Uniform as CoreUniform
+from stream_ml.pytorch.base import ModelBase
+from stream_ml.pytorch.typing import Array, NNModel
+
 from .exponential import Exponential
 from .sloped import Sloped
-from .uniform import Uniform
 
 __all__ = ["Uniform", "Sloped", "Exponential"]
+
+
+@dataclass(unsafe_hash=True)
+class Uniform(CoreUniform[Array, NNModel], ModelBase):
+    """Uniform background model."""
