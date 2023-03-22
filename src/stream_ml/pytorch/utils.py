004f5ad9ad44328cb820e8defa1fab0fa64a0029
Nathaniel Starkman <nstarman@users.noreply.github.com>
2023-03-22 12:52:49 +0100

    change import loc (#36)
    Signed-off-by: nstarman <nstarkman@protonmail.com>

--- src/stream_ml/pytorch/utils.py
+++ src/stream_ml/pytorch/utils.py
@@ -1,6 +1,5 @@
 """Utilities."""
 
-from stream_ml.core.utils.compat import array_at
-from stream_ml.core.utils.funcs import StandardScaler, within_bounds
+from stream_ml.core.utils import StandardScaler, array_at, within_bounds
 
 __all__ = ["array_at", "within_bounds", "StandardScaler"]
