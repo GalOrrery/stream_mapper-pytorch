a968bda9cfaffd852777d9a6ec397a3885e7b7c9
nstarman <nathanielstarkman@gmail.com>
2023-03-30 14:38:27 -0400

    Import param scalers from core

--- src/stream_ml/pytorch/params/__init__.py
+++ src/stream_ml/pytorch/params/__init__.py
@@ -4,15 +4,22 @@ from stream_ml.core.params import (
     ParamBounds,
     ParamNames,
     Params,
+    ParamScalers,
     freeze_params,
     set_param,
     unfreeze_params,
 )
+from stream_ml.pytorch.params import scales
 
 __all__: list[str] = [
+    # modules
+    "scales",
+    # classes
     "Params",
     "ParamNames",
     "ParamBounds",
+    "ParamScalers",
+    # functions
     "freeze_params",
     "unfreeze_params",
     "set_param",
