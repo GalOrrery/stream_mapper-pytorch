d7c674d6a1cd64d784e7553d32b4ea7c83cef942
Nathaniel Starkman <nstarman@users.noreply.github.com>
2023-05-10 10:29:36 -0400

    scipy KDE model
    Signed-off-by: Nathaniel Starkman <nstarman@users.noreply.github.com>

--- src/stream_ml/pytorch/compat/__init__.py
+++ src/stream_ml/pytorch/compat/__init__.py
@@ -1,5 +1,9 @@
 """Stream Memberships Likelihood, with ML."""
 
 from stream_ml.pytorch.compat.flow import FlowModel
+from stream_ml.pytorch.compat.kde import KDEModel
 
-__all__ = ["FlowModel"]
+__all__ = [
+    "FlowModel",
+    "KDEModel",
+]
