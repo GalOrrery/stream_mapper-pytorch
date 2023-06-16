08ad05087b749d327181a7d3c07566a80b17c0b3
Nathaniel Starkman <nstarman@users.noreply.github.com>
2023-06-16 12:03:36 -0400

    Normalizations (#84)
    * gaussian related distrubution normalization
    
    skewnorm priors still produce inf
    
    ---------
    
    Signed-off-by: Nathaniel Starkman <nstarman@users.noreply.github.com>

---
+++ src/stream_ml/pytorch/builtin/_stats/truncnorm.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+__all__: list[str] = []
+
+from typing import TYPE_CHECKING
+
+import torch as xp
+
+from stream_ml.pytorch.builtin._stats.norm import cdf as norm_cdf
+from stream_ml.pytorch.builtin._stats.norm import logpdf as norm_logpdf
+
+if TYPE_CHECKING:
+    from stream_ml.pytorch.typing import Array
+
+
+def _logpdf(
+    x: Array, /, loc: Array, sigma: Array, a: Array | float, b: Array | float
+) -> Array:
+    log_trunc = xp.log(norm_cdf(b, loc, sigma) - norm_cdf(a, loc, sigma))
+    return norm_logpdf(x, loc=loc, sigma=sigma) - log_trunc
+
+
+def logpdf(
+    x: Array,
+    /,
+    loc: Array,
+    sigma: Array,
+    a: Array | float,
+    b: Array | float,
+    *,
+    min: float = -xp.inf,
+) -> Array:
+    out = xp.full_like(x, min)
+    sel = (a <= x) & (x <= b)
+    out[sel] = _logpdf(x[sel], loc=loc[sel], sigma=sigma[sel], a=a, b=b)
+    return out
---
+++ src/stream_ml/pytorch/builtin/_stats/truncskewnorm.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+__all__: list[str] = []
+
+from typing import TYPE_CHECKING
+
+import torch as xp
+
+from stream_ml.pytorch.builtin._stats.skewnorm import cdf as skewnorm_cdf
+from stream_ml.pytorch.builtin._stats.skewnorm import logpdf as skewnorm_logpdf
+
+if TYPE_CHECKING:
+    from stream_ml.pytorch.typing import Array
+
+
+def _logpdf(
+    x: Array,
+    /,
+    loc: Array,
+    sigma: Array,
+    skew: Array,
+    a: Array | float,
+    b: Array | float,
+) -> Array:
+    log_trunc = xp.log(
+        skewnorm_cdf(b, loc, sigma, skew) - skewnorm_cdf(a, loc, sigma, skew)
+    )
+    return skewnorm_logpdf(x, loc=loc, sigma=sigma, skew=skew) - log_trunc
+
+
+def logpdf(
+    x: Array,
+    /,
+    loc: Array,
+    sigma: Array,
+    skew: Array,
+    a: Array | float,
+    b: Array | float,
+    *,
+    min: float = -xp.inf,
+) -> Array:
+    out = xp.full_like(x, min)
+    sel = (a <= x) & (x <= b)
+    out[sel] = xp.clip(
+        _logpdf(x[sel], loc=loc[sel], sigma=sigma[sel], skew=skew[sel], a=a, b=b),
+        min=min,
+    )
+    return out
