8a657182bfdc61e6c5a4a2dc7aa67bd717d65a43
Connor MacKeigan <comackeigan@outlook.com>
2023-06-20 12:46:02 -0400

    Control Point Prior Bugfix
    Changing stacking dimensions of control point parameter array to match model parameter dimentions.
    
    Signed-off-by: Connor MacKeigan <comackeigan@outlook.com>

--- src/stream_ml/pytorch/prior/_track.py
+++ src/stream_ml/pytorch/prior/_track.py
@@ -211,7 +211,7 @@ class ControlRegions(TrackPriorBase):
         """
         # Get model parameters evaluated at the control points. shape (C, 1).
         cmpars = model.unpack_params(model(self._x))  # type: ignore[call-overload]  # noqa: E501
-        cmp_arr = xp.hstack(  # (C, F)
+        cmp_arr = xp.stack(  # (C, F)
             tuple(cmpars[(n, self.component_param_name)] for n in self._y_names)
         )
 
