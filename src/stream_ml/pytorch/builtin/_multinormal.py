f5a32e0be59c9de8288379ef49c9105b23429e7f
nstarman <nstarman@users.noreply.github.com>
2023-07-06 18:06:09 -0400

    use _stack_param
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/stream_ml/pytorch/builtin/_multinormal.py
+++ src/stream_ml/pytorch/builtin/_multinormal.py
@@ -98,11 +98,9 @@ class MultivariateMissingNormal(MultivariateNormal):  # (MultivariateNormal)
             Additional arguments.
         """
         # Normal
-        datav = data[self.coord_names].array
-        mu = self.xp.stack([mpars[c, "mu"] for c in self.coord_names], 1)
-        sigma = self.xp.stack(
-            [self.xp.exp(mpars[c, "ln-sigma"]) for c in self.coord_names], 1
-        )
+        x = data[self.coord_names].array
+        mu = self._stack_params(mpars, "mu", self.coord_names)
+        sigma = self.xp.exp(self._stack_params(mpars, "ln-sigma", self.coord_names))
 
         indicator: Array
         if mask is not None:
@@ -111,14 +109,14 @@ class MultivariateMissingNormal(MultivariateNormal):  # (MultivariateNormal)
             msg = "mask is required"
             raise ValueError(msg)
         else:
-            indicator = xp.ones_like(datav, dtype=xp.int)
+            indicator = xp.ones_like(x, dtype=xp.int)
             # shape (1, F) so that it can broadcast with (N, F)
 
         # misc
         dimensionality = indicator.sum(dim=1)  # (N,)
 
         # Data - model
-        dmm = indicator * (datav - mu)  # (N, F)
+        dmm = indicator * (x - mu)  # (N, F)
 
         # Covariance related
         cov = indicator * sigma**2  # (N, F) positive definite
