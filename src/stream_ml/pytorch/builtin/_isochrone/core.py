f0929ec089daea88e48cbe58634741f74028eb14
nstarman <nstarman@users.noreply.github.com>
2023-08-03 21:50:59 -0700

    refactor for where
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/stream_ml/pytorch/builtin/_isochrone/core.py
+++ src/stream_ml/pytorch/builtin/_isochrone/core.py
@@ -2,16 +2,16 @@
 
 from __future__ import annotations
 
-__all__: list[str] = []
+__all__ = ["IsochroneMVNorm"]
 
 from dataclasses import KW_ONLY, dataclass, field
 from functools import reduce
 from typing import TYPE_CHECKING, Any
 
 import torch as xp
-from torch.distributions import MultivariateNormal
 
 from stream_ml.core._core.field import NNField
+from stream_ml.core.builtin._utils import WhereRequiredError
 from stream_ml.core.utils.frozen_dict import FrozenDict, FrozenDictField
 from stream_ml.core.utils.funcs import within_bounds
 
@@ -31,8 +31,7 @@ if TYPE_CHECKING:
     from stream_ml.pytorch.typing import Array, NNModel
 
 
-# =============================================================================
-# Isochrone Model
+_log2pi = xp.log(xp.asarray(2 * xp.pi))
 
 
 @dataclass(unsafe_hash=True)
@@ -137,10 +136,10 @@ class IsochroneMVNorm(ModelBase):
 
         # phot_err_names must be None or the same length as phot_names.
         # we can't check that the names are the same, because they aren't.
-        kmen = self.phot_err_names
-        if kmen is not None and len(kmen) != len(self.phot_names):
+        kpen = self.phot_err_names
+        if kpen is not None and len(kpen) != len(self.phot_names):
             msg = (
-                f"`phot_err_names` ({kmen}) must be None or "
+                f"`phot_err_names` ({kpen}) must be None or "
                 f"the same length as `phot_names` ({self.phot_names})."
             )
             raise ValueError(msg)
@@ -172,7 +171,9 @@ class IsochroneMVNorm(ModelBase):
         # Midpoint of gamma edges array
         self._gamma_points: Array = (self.gamma_edges[:-1] + self.gamma_edges[1:]) / 2
         # Points on the isochrone along gamma  # ([N], I, F)
-        isochrone_locs = xp.asarray(self.isochrone_spl(self._gamma_points))
+        isochrone_locs = xp.asarray(
+            self.isochrone_spl(self._gamma_points), dtype=self._gamma_points.dtype
+        )
         self._isochrone_locs = isochrone_locs[None, :, :]
         # And errors  ([N], I, F, F)
         if self.isochrone_err_spl is None:
@@ -180,6 +181,16 @@ class IsochroneMVNorm(ModelBase):
         else:
             isochrone_err = xp.asarray(self.isochrone_err_spl(self._gamma_points))
             self._isochrone_cov = xp.diag_embed(isochrone_err[None, :, :])
+
+    @property
+    def nI(self) -> int:  # noqa: N802
+        """The number of isochrone points."""
+        return len(self._gamma_points)
+
+    @property
+    def nF(self) -> int:  # noqa: N802
+        """The number of photometric features."""
+        return len(self.phot_names)
 
     def _phot_in_bound(self, data: Data[Array], /) -> Array:
         """Elementwise log prior for coordinate bounds.
@@ -203,7 +214,9 @@ class IsochroneMVNorm(ModelBase):
         /,
         data: Data[Array],
         *,
+        where: Data[Array] | None = None,
         correlation_matrix: Array | None = None,
+        correlation_det: Array | None = None,
         **kwargs: Array,
     ) -> Array:
         r"""Compute the log-likelihood.
@@ -217,18 +230,30 @@ class IsochroneMVNorm(ModelBase):
             - distmod, ln-sigma : [mag]
 
         data : Data[Array[(N,)]]
-            The data. Must contain the fields in ``phot_names`` and ``phot_err_names``.
+            The data. Must contain the fields in ``phot_names`` and
+            ``phot_err_names``.
 
-        correlation_matrix : Array[(N,F,F)], optional keyword-only
-            The correlation matrix. If not provided, then the covariance matrix is
-            assumed to be diagonal.
-            The covariance matrix is computed as:
+        where : Data[Array[(N,), bool]] | None, optional keyword-only
+            Where to evaluate the log-likelihood. If not provided, then the
+            log-likelihood is evaluated at all data points. ``where`` must
+            contain the fields in ``phot_names``. Each field must be a boolean
+            array of the same length as `data`. `True` indicates that the data
+            point is available, and `False` indicates that the data point is not
+            available.
+
+        correlation_matrix : Array[(N,F,F)] | None, optional keyword-only
+            The correlation matrix. If not provided, then the covariance matrix
+            is assumed to be diagonal. The covariance matrix is computed as:
 
             .. math::
 
                 \rm{cov}(X) =       \rm{diag}(\vec{\sigma})
-                              \cdot \rm{corr}
-                              \cdot \rm{diag}(\vec{\sigma})
+                              \cdot \rm{corr} \cdot \rm{diag}(\vec{\sigma})
+        correlation_det: Array[(N,)] | None, optional keyword-only
+            The determinant of the correlation matrix. If not provided, then
+            the determinant is only the product of the diagonal elements of the
+            covariance matrix.
+
         **kwargs: Array
             Not used.
 
@@ -236,12 +261,24 @@ class IsochroneMVNorm(ModelBase):
         -------
         Array[(N,)]
         """
-        dm = mpars[("distmod", "mu")]  # (N,)
-        dm_sigma = self.xp.exp(mpars[("distmod", "ln-sigma")])  # (N,)
+        # 'where' is used to indicate which data points are available. If
+        # 'where' is not provided, then all data points are assumed to be
+        # available.
+        where_: Array  # (N, F)
+        if where is not None:
+            where_ = where[tuple(self.phot_names)].array
+        elif self.require_where:
+            raise WhereRequiredError
+        else:
+            where_ = self.xp.ones((len(data), self.nF), dtype=bool)
+
+        if correlation_matrix is not None and correlation_det is None:
+            msg = "Must provide `correlation_det`."
+            raise ValueError(msg)
 
         # Mean : isochrone + distance modulus
-        # ([N], I, F) + (N, [I], [F]) = (N, I, F)
-        mean = self.xp.zeros((len(dm), 1, 1)) + self._isochrone_locs
+        dm = mpars[("distmod", "mu")]  # (N,)
+        mean = self.xp.zeros((len(dm), 1, 1)) + self._isochrone_locs  # (N, I, F)
         mean[:, :, self.phot_apply_dm] += dm[:, None, None]
 
         # Compute what gamma is observable for each star
@@ -256,27 +293,60 @@ class IsochroneMVNorm(ModelBase):
         )
 
         # Covariance: star (N, [I], F, F)
-        vars_data = (
-            xp.diag_embed(data[self.phot_err_names].array)[:, None, :, :]
+        stds = data[self.phot_err_names].array
+        std_data = (
+            xp.diag_embed(stds)[:, None, :, :]
             if self.phot_err_names is not None
             else self.xp.zeros(1)
         )
+        # Covariance data "The covariance matrix can be written as the rescaling
+        # of a correlation matrix by the marginal variances:"
+        # (https://en.wikipedia.org/wiki/Covariance_matrix#Correlation_matrix)
         cov_data = (
-            vars_data @ vars_data
+            std_data**2
             if correlation_matrix is None
-            else vars_data @ correlation_matrix[:, None, :, :] @ vars_data
+            else std_data @ correlation_matrix[:, None, :, :] @ std_data
         )
         # Covariance: isochrone ([N], I, F, F)
+        dm_sigma = self.xp.exp(mpars[("distmod", "ln-sigma")])  # (N,)
         # Covariance: distance modulus  (N, [I], F, F)
-        cov_dm = xp.diag_embed(
-            xp.ones((len(data), len(self.phot_names))) * dm_sigma[:, None] ** 2
-        )[:, None, :, :]
-        cov = cov_data + self._isochrone_cov + cov_dm
+        cov_dm = xp.diag_embed(xp.ones((len(data), self.nF)) * dm_sigma[:, None] ** 2)[
+            :, None, :, :
+        ]
+        # The covariance, setting non-observed dimensions to 0.
+        # (N, I, F, F) positive definite.
+        idx_cov = xp.diag_embed(where_.to(dtype=data.dtype))[:, None]  # (N, I, F, F)
+        cov = idx_cov @ (cov_data + self._isochrone_cov + cov_dm) @ idx_cov
+        # The determinant, dropping the dimensionality of non-observed
+        # dimensions.
+        logdet = xp.log(
+            xp.linalg.det(cov + (xp.eye(self.nF)[None, None] - idx_cov))
+        )  # (N, [I])
 
-        # Log-likelihood of the multivariate normal
-        mvn = MultivariateNormal(mean, covariance_matrix=cov)
-        mdata = data[self.phot_names].array[:, None, ...]  # (N, [I], F)
-        lnliks = mvn.log_prob(mdata)  # (N, I)
+        # Dimensionality, dropping missing dimensions (N, [I])
+        D = where_.sum(dim=-1)  # noqa: N806
+
+        # Construct the data - mean (N, I, F), setting non-observed dimensions to 0.
+        sel = where_[:, None, :].expand(-1, self.nI, -1)
+        x = xp.zeros((len(data), self.nI, self.nF), dtype=data.dtype)
+        x[sel] = (
+            data[self.phot_names].array[:, None, :].expand(-1, self.nI, -1)[sel]
+            - mean[sel]
+        )
+
+        lnliks = xp.zeros((len(data), len(self._gamma_points)))  # (N, I)
+        lnliks = -0.5 * (  # (N, I, 1, 1) -> (N, I)
+            D[:, None] * _log2pi
+            + logdet
+            + (
+                x[:, :, None, :]  # (N, I, 1, F)
+                @ xp.linalg.pinv(cov)  # (N, I, F, F)
+                @ x[..., None]  # (N, I, F, 1)
+            )[..., 0, 0]
+        )
+        # mvn = MultivariateNormal(mean, covariance_matrix=cov)
+        # mdata = data[self.phot_names].array[:, None, ...]  # (N, [I], F)
+        # lnliks = mvn.log_prob(mdata)  # (N, I)
 
         # log PDF: the (log)-Reimannian sum over the isochrone (log)-pdfs:
         # sum_i(deltagamma_i PDF(gamma_i) * Pgamma)  -> translated to log_pdf
