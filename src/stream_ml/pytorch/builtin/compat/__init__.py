ff91ea4954f85f43a666b9cd3b6e9a1777508766
nstarman <nstarman@users.noreply.github.com>
2023-09-06 18:34:52 -0400

    Add flow for zuko package
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/stream_ml/pytorch/builtin/compat/__init__.py
+++ src/stream_ml/pytorch/builtin/compat/__init__.py
@@ -1,9 +1,11 @@
 """Stream Memberships Likelihood, with ML."""
 
-from stream_ml.pytorch.builtin.compat.flow import FlowModel
 from stream_ml.pytorch.builtin.compat.kde import KDEModel
+from stream_ml.pytorch.builtin.compat.nflow import NFlowModel
+from stream_ml.pytorch.builtin.compat.zuko import ZukoFlowModel
 
 __all__ = [
-    "FlowModel",
+    "ZukoFlowModel",
+    "NFlowModel",
     "KDEModel",
 ]
