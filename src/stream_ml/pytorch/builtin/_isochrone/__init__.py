913ddc7f834fa27c5b244c236903b6ceada78eae
Nathaniel Starkman <nstarman@users.noreply.github.com>
2023-10-11 17:18:03 -0400

    move isochrone stuff to core (#152)

--- src/stream_ml/pytorch/builtin/_isochrone/__init__.py
+++ src/stream_ml/pytorch/builtin/_isochrone/__init__.py
@@ -2,15 +2,6 @@
 
 from __future__ import annotations
 
-from stream_ml.pytorch.builtin._isochrone.core import IsochroneMVNorm
-from stream_ml.pytorch.builtin._isochrone.mf import (
-    HardCutoffMassFunction,
-    StepwiseMassFunction,
-    StreamMassFunction,
-    UniformStreamMassFunction,
-)
-from stream_ml.pytorch.builtin._isochrone.utils import Parallax2DistMod
-
 __all__: list[str] = [
     # Mass Function
     "StreamMassFunction",
@@ -22,3 +13,30 @@ __all__: list[str] = [
     # Utils
     "Parallax2DistMod",
 ]
+
+from dataclasses import field, make_dataclass
+
+import torch as xp
+
+from stream_ml.core.builtin._isochrone.mf import (
+    HardCutoffMassFunction,
+    StepwiseMassFunction,
+    StreamMassFunction,
+    UniformStreamMassFunction,
+)
+from stream_ml.core.builtin._isochrone.utils import (
+    Parallax2DistMod as CoreParallax2DistMod,
+)
+
+from stream_ml.pytorch.builtin._isochrone.core import IsochroneMVNorm
+from stream_ml.pytorch.typing import Array, ArrayNamespace
+
+# -----------------------------------------------------------------------------
+
+Parallax2DistMod = make_dataclass(
+    "Parallax2DistMod",
+    [("array_namespace", ArrayNamespace[Array], field(default=xp, kw_only=True))],
+    bases=(CoreParallax2DistMod[Array],),
+    unsafe_hash=True,
+    repr=False,
+)
